57393c4cba287bdc8b499af645aa73133e4a3f36
Kurt Strecker <ksphotopro@gmail.com>
2021-05-12 22:32:46 -0400

    cleaned up and commented

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,10 @@
 #![no_main]
 
 extern crate panic_itm;
-use cortex_m::{asm, singleton};
+use cortex_m::{singleton};
 use cortex_m_rt::entry;
 use stm32f3_discovery::stm32f3xx_hal as hal;
-use hal::{pac, prelude::*, stm32, serial::Serial, rcc, gpio::gpioa, gpio::AF7};
-
-// use stm32f3_discovery::stm32f3xx_hal::{self as hal, pac, prelude::*};
-
-const BUFSIZE: usize = 15;
-const X_OFFSET:f32 = -47.0;
-const Y_OFFSET:f32 = 222.5;
-const Z_OFFSET:f32 = -15.5;
+use hal::{pac, prelude::*, stm32, serial::Serial, rcc};
 
 struct SerialStruct {
     tx: hal::serial::Tx<stm32::USART1>,
@@ -24,24 +17,37 @@ struct SerialStruct {
 
 impl SerialStruct {
     fn new() -> Self {
-        let dp = pac::Peripherals::take().unwrap();
+        // get peripherals
+        let device_periphs = pac::Peripherals::take().unwrap();
         let core_periphs = cortex_m::Peripherals::take().unwrap();
-        let pac::Peripherals { RCC: destruct_RCC, DMA1: destruct_DMA1, FLASH: destruct_FLASH, GPIOA: destruct_gpioa, .. } = dp;
-        let mut rcc = destruct_RCC.constrain();
+        let pac::Peripherals { RCC: destruct_RCC, DMA1: destruct_DMA1, FLASH: destruct_FLASH, GPIOA: destruct_gpioa, .. } = device_periphs;
+
+        // setup rcc
+        let rcc = destruct_RCC.constrain();
         let rcc::Rcc { mut ahb, mut apb2, .. } = rcc;
+
+        // get dma1
         let dma1 = destruct_DMA1.split(&mut ahb);
         let  hal::dma::dma1::Channels { ch4, ch5, ..} = dma1;
+
         let mut flash = destruct_FLASH.constrain();
         let mut gpioa = destruct_gpioa.split(&mut ahb);
+
         let clocks = rcc.cfgr.freeze(&mut flash.acr);
-        let mut delay = hal::delay::Delay::new(core_periphs.SYST, clocks);
+        let delay = hal::delay::Delay::new(core_periphs.SYST, clocks);
 
         let pins = (
             gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
             gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
         );
 
-        let serial = Serial::usart1(dp.USART1, pins, 115200.bps(), clocks, &mut apb2);
+        let serial = Serial::usart1(
+            device_periphs.USART1,
+            pins,
+            115200.bps(),
+            clocks,
+            &mut apb2);
+
         let (tx, rx) = serial.split();
 
         SerialStruct {
@@ -56,8 +62,9 @@ impl SerialStruct {
 
 #[entry]
 fn main() -> ! {
+
     let mut serial_handler = SerialStruct::new();
-    let mut initial_buf = singleton!(: [u8; 9] = *b"hello DMA").unwrap();
+    let mut initial_buf = singleton!(: [u8; 9] = *b"hi DMA!\r\n").unwrap();
 
     loop {
         let sending = serial_handler.tx.write_all(initial_buf, serial_handler.tx_channel);
